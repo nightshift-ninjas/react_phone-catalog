55c8c4884134e968aa9edf3bd65273fa4407831e
Nazariy <nazariy.holovach2007@gmail.com>
2025-11-15 19:30:32 +0200

    chore: add base folder structure

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import './App.scss';
-
-export const App = () => (
-  <div className="App">
-    
-  </div>
-);
---
+++ src/features/home/HomePage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return <h1>This is HomePage</h1>;
+};
+
+export default HomePage;
