b3f9b0c5937005e44c67db777a85d28d388df907
Nazariy <nazariy.holovach2007@gmail.com>
2025-11-17 14:27:25 +0200

    NG-16 style: style the auth page

--- src/features/auth/LoginPage/LoginPage.tsx
+++ src/features/auth/LoginPage/LoginPage.tsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
 import { authClient } from '../../../services/auth/auth.service';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { Form } from 'radix-ui';
+import { PasswordField } from '../../../shared/ui/PasswordField';
+import GoogleIcon from '../../../shared/assets/icons/google.svg?react';
+import '../form.scss';
 
 export function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
+  const [agreement, setAgreement] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const navigate = useNavigate();
 
@@ -43,38 +48,92 @@ export function LoginPage() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Login</h2>
+    <Form.Root className="form" onSubmit={handleSubmit}>
+      <h2 className="form__title">Log in your account</h2>
 
-      <div>
+      <p className="form__text">
+        Don&apos;t have an account?
+        <Link to="/auth/signup" className="form__link">
+          Create account
+        </Link>
+      </p>
+
+      <Form.Field className="form__field" name="email">
+        <Form.Control asChild>
+          <input
+            className="form__input"
+            type="email"
+            placeholder="Email..."
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required
+          />
+        </Form.Control>
+
+        <div>
+          <Form.Message className="form__message" match="typeMismatch">
+            Please provide a valid email
+          </Form.Message>
+        </div>
+      </Form.Field>
+
+      <Form.Field className="form__field" name="password">
+        <Form.Control asChild>
+          <PasswordField
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            placeholder="Password..."
+            name="password"
+            required
+          />
+        </Form.Control>
+
+        <div>
+          <Form.Message className="form__message" match="typeMismatch">
+            Please provide a valid password
+          </Form.Message>
+        </div>
+      </Form.Field>
+
+      <div className="form__checkbox-wrapper">
         <input
-          type="email"
-          placeholder="Email..."
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-          required
+          id="agreement"
+          className="form__checkbox"
+          type="checkbox"
+          checked={agreement}
+          onChange={(event) => setAgreement(event.target.checked)}
         />
+        <label htmlFor="agreement">
+          I agree
+          <a href="#" target="_blank" className="form__link">
+            Terms & Conditions
+          </a>
+        </label>
       </div>
 
-      <div>
-        <input
-          type="password"
-          placeholder="Password..."
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-          required
-        />
-      </div>
+      <Form.Submit asChild>
+        <button className="form__submit" disabled={loading || !agreement}>
+          Log in
+        </button>
+      </Form.Submit>
 
-      <button type="submit" disabled={loading}>
-        {loading ? 'Logging in...' : 'Login'}
+      {error && <span className="form__message">{error}</span>}
+
+      <p className="form__divider">
+        <span className="form__line"></span>
+        <span>Or login with</span>
+        <span className="form__line"></span>
+      </p>
+
+      <button
+        className="form__button-google"
+        type="button"
+        onClick={handleGoogleLogin}
+        disabled={loading}
+      >
+        <GoogleIcon />
+        {loading ? 'Logging in...' : 'Google'}
       </button>
-
-      <button type="button" onClick={handleGoogleLogin} disabled={loading}>
-        {loading ? 'Logging in with Google...' : 'Login with Google'}
-      </button>
-
-      {error && <p>{error}</p>}
-    </form>
+    </Form.Root>
   );
 }
--- src/features/auth/SignupPage/SignupPage.tsx
+++ src/features/auth/SignupPage/SignupPage.tsx
@@ -1,11 +1,16 @@
-import { useState } from "react";
-import { authClient } from "../../../services/auth/auth.service";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import { authClient } from '../../../services/auth/auth.service';
+import { Link, useNavigate } from 'react-router-dom';
+import { Form } from 'radix-ui';
+import { PasswordField } from '../../../shared/ui/PasswordField';
+import GoogleIcon from '../../../shared/assets/icons/google.svg?react';
+import '../form.scss';
 
 export function SignupPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
+  const [agreement, setAgreement] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const navigate = useNavigate();
 
@@ -16,7 +21,7 @@ export function SignupPage() {
 
     try {
       await authClient.register(email, password);
-      navigate("/auth/login");
+      navigate('/auth/login');
     } catch (error) {
       setError(`Someting went wrong while trying to signup ${error}`);
     } finally {
@@ -24,35 +29,105 @@ export function SignupPage() {
     }
   };
 
+  const handleGoogleSignup = async () => {
+    setLoading(true);
+    try {
+      await authClient.loginWithGoogle();
+      navigate('/');
+    } catch (error) {
+      setError(`Google login failed. Please try again: ${error}`);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <form onSubmit={handleSubmit} style={{ maxWidth: 400 }}>
-      <h2>Signup</h2>
+    <Form.Root className="form" onSubmit={handleSubmit}>
+      <h2 className="form__title">Create an account</h2>
 
-      <div>
+      <p className="form__text">
+        Already have an account?
+        <Link to="/auth/login" className="form__link">
+          Log in
+        </Link>
+      </p>
+
+      <Form.Field className="form__field" name="email">
+        <Form.Control asChild>
+          <input
+            className="form__input"
+            type="email"
+            placeholder="Email..."
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required
+          />
+        </Form.Control>
+
+        <div>
+          <Form.Message className="form__message" match="typeMismatch">
+            Please provide a valid email
+          </Form.Message>
+        </div>
+      </Form.Field>
+
+      <Form.Field className="form__field" name="password">
+        <Form.Control asChild>
+          <PasswordField
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            placeholder="Password..."
+            name="password"
+            required
+          />
+        </Form.Control>
+
+        <div>
+          <Form.Message className="form__message" match="typeMismatch">
+            Please provide a valid password
+          </Form.Message>
+        </div>
+      </Form.Field>
+
+      <div className="form__checkbox-wrapper">
         <input
-          type="email"
-          placeholder="Email..."
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-          required
+          id="agreement"
+          className="form__checkbox"
+          type="checkbox"
+          checked={agreement}
+          onChange={(event) => setAgreement(event.target.checked)}
         />
+        <label htmlFor="agreement">
+          I agree
+          <a href="#" target="_blank" className="form__link">
+            Terms & Conditions
+          </a>
+        </label>
       </div>
 
-      <div>
-        <input
-          placeholder="Password..."
-          type="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-          required
-        />
-      </div>
+      <Form.Submit asChild>
+        <button className="form__submit" disabled={loading || !agreement}>
+          Create account
+        </button>
+      </Form.Submit>
 
-      <button type="submit" disabled={loading}>
-        {loading ? "Signing up..." : "Signup"}
+      {error && <span className="form__message">{error}</span>}
+
+      <p className="form__divider">
+        <span className="form__line"></span>
+        <span>Or register with</span>
+        <span className="form__line"></span>
+      </p>
+
+      <button
+        className="form__button-google"
+        type="button"
+        onClick={handleGoogleSignup}
+        disabled={loading}
+      >
+        <GoogleIcon />
+        {loading ? 'Logging in...' : 'Google'}
       </button>
-
-      {error && <p>{error}</p>}
-    </form>
+    </Form.Root>
   );
 }
