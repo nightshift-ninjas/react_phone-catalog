1c46e2582b3d0788ccb5f95816437a91d658bbce
Viktoriia <vikapitsan4@gmail.com>
2025-11-18 10:24:37 +0200

    feat(NG-11): create the ColorButton component

---
+++ src/features/product-detail/components/ColorButton/ColorTypes.ts
@@ -0,0 +1,18 @@
+export type Colors =
+  | 'black'
+  | 'blue'
+  | 'green'
+  | 'yellow'
+  | 'pink'
+  | 'gold'
+  | 'silver';
+
+export const COLORS: Record<Colors, string> = {
+  black: "#000000",
+  blue: "#007AFF",
+  green: "#34C759",
+  yellow: "#FFCC00",
+  pink: "#FF2D55",
+  gold: "#FCDBC1",
+  silver: "#C0C0C0",
+};
