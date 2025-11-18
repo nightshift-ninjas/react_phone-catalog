81d9aa0180b4b16ff0ba0aa0225021270acb805c
Andrii Linkevych <andrii.linkevych.21@pnu.edu.ua>
2025-11-18 13:39:27 +0200

    NG-14 feat: add ProductSlider component and change home page, button and navbar

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ProductSlider } from '../../widgets/ProductSlider';
+import { productService } from '../../services/product/product.service';
+import type { Product } from '../../services/product';
 
 const HomePage: React.FC = () => {
-  return <h1>This is HomePage</h1>;
+  const [products, setProducts] = useState<Product[] | null>(null);
+  useEffect(() => {
+    const loadProducts = async () => {
+      try {
+        const productsFromServer = await productService.fetchAll(); 
+        setProducts(productsFromServer);
+      } catch (error) {
+        console.log(error);
+        throw new Error("Something went wrong during loading data");
+      }
+    };
+
+    loadProducts();
+  }, []);
+
+  return (
+    <>
+      {products !== null && <ProductSlider layoutText='Default' products={products} />}
+    </>
+  );
 };
 
 export default HomePage;
