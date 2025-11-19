d62a97b41cb556b789702c3e30557b9e1edbf71e
Marina Kramarchuk <kramarchukmm20182018@gmail.com>
2025-11-19 13:30:04 +0200

    fix(NG-190): made the product name clickable

--- src/features/cart/components/CartItem/CartItem.tsx
+++ src/features/cart/components/CartItem/CartItem.tsx
@@ -4,33 +4,32 @@ import type { CartItem as CartItemType } from '../../../../services/cart';
 import { cartService } from '../../../../services/cart/cart.services';
 import { QuantityControl } from '../QuantityControl/QuantityControl';
 import { BASE_URL } from '../../../../widgets/ProductCard';
+import { Link } from 'react-router-dom';
 
 export type Props = {
   item: CartItemType;
-}
+};
 
 export const CartItem: React.FC<Props> = ({ item }) => {
   const [quantity, setQuantity] = useState(item.quantity);
 
   const handleRemove = async () => {
-  try {
-    await cartService.removeCartItemById(item.id);
-  } catch (error) {
-    console.error("Failed to remove cart item:", error);
-  }
-};
-
+    try {
+      await cartService.removeCartItemById(item.id);
+    } catch (error) {
+      console.error('Failed to remove cart item:', error);
+    }
+  };
 
   const handleIncrease = async () => {
     try {
       const updated = { ...item, quantity: quantity + 1 };
       await cartService.updateCartItem(item.id, updated);
-      setQuantity(q => q + 1);
+      setQuantity((q) => q + 1);
     } catch (error) {
-      console.error("Failed to increase quantity:", error);
+      console.error('Failed to increase quantity:', error);
     }
   };
-
 
   const handleDecrease = async () => {
     if (quantity === 1) return;
@@ -38,33 +37,33 @@ export const CartItem: React.FC<Props> = ({ item }) => {
     try {
       const updated = { ...item, quantity: quantity - 1 };
       await cartService.updateCartItem(item.id, updated);
-      setQuantity(q => q - 1);
+      setQuantity((q) => q - 1);
     } catch (error) {
-      console.error("Failed to decrease quantity:", error);
+      console.error('Failed to decrease quantity:', error);
     }
   };
 
   return (
     <div className="cart-item">
-
       <div className="cart-item__top">
-        <button
-          className="cart-item__remove"
-          onClick={handleRemove}
-        >
+        <button className="cart-item__remove" onClick={handleRemove}>
           ×
         </button>
 
         <div className="cart-item__image_wrapper">
           <img
-          src={`${BASE_URL}${item.product?.images?.[0] ?? 'placeholder.png'}`}
-          alt={item.product?.name || 'Product image'}
-          className="cart-item__image"
-        />
+            src={`${BASE_URL}${item.product?.images?.[0] ?? 'placeholder.png'}`}
+            alt={item.product?.name || 'Product image'}
+            className="cart-item__image"
+          />
         </div>
 
-        <div className="cart-item__title">{item.product?.name}</div>
-
+        <Link
+          to={`/catalog/${item.product?.category}/product/${item.product?.id}`}
+          className="cart-item__title"
+        >
+          {item.product?.name}
+        </Link>
       </div>
 
       <div className="cart-item__bottom">
