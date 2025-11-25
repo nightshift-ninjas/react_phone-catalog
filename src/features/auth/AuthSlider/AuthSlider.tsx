0804faed16e64941782b5c004e429b9a7504cfc5
Andrii Linkevych <andrii.linkevych.21@pnu.edu.ua>
2025-11-25 13:14:42 +0200

    NG-Amim feat: Add animatioin to auth page, not found page, prod cart and add autoplay to slider on auth page

--- src/features/auth/AuthSlider/AuthSlider.tsx
+++ src/features/auth/AuthSlider/AuthSlider.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper/modules';
+import { Autoplay, Pagination } from 'swiper/modules';
 import { Link } from 'react-router-dom';
 import AuthArrowIcon from '../../../shared/assets/icons/auth-arrow.svg?react';
 import './AuthSlider.scss';
@@ -8,37 +8,47 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { useTranslation } from 'react-i18next';
+import { SlideIn } from '../../../shared/animation/SlideIn';
 
 type Props = {
   slides: string[];
 };
 
 export const AuthSlider: React.FC<Props> = ({ slides }) => {
-  const { t } = useTranslation('auth')
+  const { t } = useTranslation('auth');
   return (
-    <div className="auth-slider">
-      <Swiper
-        modules={[Pagination]}
-        pagination={{ clickable: true }}
-        spaceBetween={20}
-        slidesPerView={1}
-        className="auth-slider__swiper"
-      >
-        {slides.map((slide, index) => (
-          <SwiperSlide key={index} className="auth-slider__slide">
-            <img
-              src={slide}
-              alt={`${t('slideIndex')} ${index}`}
-              className="auth-slider__image"
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    <SlideIn
+      beforeAnimationState={{
+        x: -50,
+        y: 0,
+        opacity: 0,
+      }}
+    >
+      <div className="auth-slider">
+        <Swiper
+          modules={[Pagination, Autoplay]}
+          pagination={{ clickable: true }}
+          spaceBetween={20}
+          slidesPerView={1}
+          className="auth-slider__swiper"
+          autoplay={{ delay: 5000 }}
+        >
+          {slides.map((slide, index) => (
+            <SwiperSlide key={index} className="auth-slider__slide">
+              <img
+                src={slide}
+                alt={`${t('slideIndex')} ${index}`}
+                className="auth-slider__image"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
 
-      <Link className="auth-slider__button" to="/">
-        {t("backToWebsite")}
-        <AuthArrowIcon />
-      </Link>
-    </div>
+        <Link className="auth-slider__button" to="/">
+          {t('backToWebsite')}
+          <AuthArrowIcon />
+        </Link>
+      </div>
+    </SlideIn>
   );
 };
--- src/features/auth/SignupPage/SignupPage.tsx
+++ src/features/auth/SignupPage/SignupPage.tsx
@@ -8,6 +8,7 @@ import { LanguageContext } from '../../../shared/context/language';
 import { ROUTES } from '../../../shared/config/routes';
 import '../form.scss';
 import { useTranslation } from 'react-i18next';
+import { SlideIn } from '../../../shared/animation/SlideIn';
 
 export function SignupPage() {
   const { language: lng } = useContext(LanguageContext)!;
@@ -52,128 +53,130 @@ export function SignupPage() {
   };
 
   return (
-    <Form.Root className="form" onSubmit={handleSubmit}>
-      <h2 className="form__title">{t('createAccountTitle')}</h2>
+    <SlideIn>
+      <Form.Root className="form" onSubmit={handleSubmit}>
+        <h2 className="form__title">{t('createAccountTitle')}</h2>
 
-      <p className="form__text">
-        {t('alreadyHaveAccountPrompt')}{' '}
-        <Link to={`/${lng}/${ROUTES.login}`} className="form__link">
-          {t('loginButton')}
-        </Link>
-      </p>
+        <p className="form__text">
+          {t('alreadyHaveAccountPrompt')}{' '}
+          <Link to={`/${lng}/${ROUTES.login}`} className="form__link">
+            {t('loginButton')}
+          </Link>
+        </p>
 
-      <div className="form__field-group">
-        <Form.Field className="form__field" name="firstName">
+        <div className="form__field-group">
+          <Form.Field className="form__field" name="firstName">
+            <Form.Control asChild>
+              <input
+                className="form__input"
+                type="text"
+                placeholder={t('firstNamePlaceholder')}
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                required
+              />
+            </Form.Control>
+            <div>
+              <Form.Message className="form__message" match="valueMissing">
+                {t('firstNameValidationFailed')}
+              </Form.Message>
+            </div>
+          </Form.Field>
+
+          <Form.Field className="form__field" name="lastName">
+            <Form.Control asChild>
+              <input
+                className="form__input"
+                type="text"
+                placeholder={t('lastNamePlaceholder')}
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                required
+              />
+            </Form.Control>
+            <div>
+              <Form.Message className="form__message" match="valueMissing">
+                {t('lastNameValidationFailed')}
+              </Form.Message>
+            </div>
+          </Form.Field>
+        </div>
+
+        <Form.Field className="form__field" name="email">
           <Form.Control asChild>
             <input
               className="form__input"
-              type="text"
-              placeholder={t('firstNamePlaceholder')}
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
+              type="email"
+              placeholder={t('emailPlaceholder')}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
           </Form.Control>
           <div>
-            <Form.Message className="form__message" match="valueMissing">
-              {t('firstNameValidationFailed')}
+            <Form.Message className="form__message" match="typeMismatch">
+              {t('emailValidationFailed')}
             </Form.Message>
           </div>
         </Form.Field>
 
-        <Form.Field className="form__field" name="lastName">
+        <Form.Field className="form__field" name="password">
           <Form.Control asChild>
-            <input
-              className="form__input"
-              type="text"
-              placeholder={t('lastNamePlaceholder')}
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
+            <PasswordField
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder={t('passwordPlaceholder')}
+              name="password"
               required
             />
           </Form.Control>
           <div>
-            <Form.Message className="form__message" match="valueMissing">
-              {t('lastNameValidationFailed')}
+            <Form.Message className="form__message" match="typeMismatch">
+              {t('passwordValidationFailed')}
             </Form.Message>
           </div>
         </Form.Field>
-      </div>
 
-      <Form.Field className="form__field" name="email">
-        <Form.Control asChild>
+        <div className="form__checkbox-wrapper">
           <input
-            className="form__input"
-            type="email"
-            placeholder={t('emailPlaceholder')}
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
+            id="agreement"
+            className="form__checkbox"
+            type="checkbox"
+            checked={agreement}
+            onChange={(e) => setAgreement(e.target.checked)}
           />
-        </Form.Control>
-        <div>
-          <Form.Message className="form__message" match="typeMismatch">
-            {t('emailValidationFailed')}
-          </Form.Message>
+          <label htmlFor="agreement">
+            {t('iAgreeCheckbox')}{' '}
+            <a href="#" target="_blank" className="form__link">
+              {t('termsAndConditions')}
+            </a>
+          </label>
         </div>
-      </Form.Field>
 
-      <Form.Field className="form__field" name="password">
-        <Form.Control asChild>
-          <PasswordField
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder={t('passwordPlaceholder')}
-            name="password"
-            required
-          />
-        </Form.Control>
-        <div>
-          <Form.Message className="form__message" match="typeMismatch">
-            {t('passwordValidationFailed')}
-          </Form.Message>
-        </div>
-      </Form.Field>
+        <Form.Submit asChild>
+          <button className="form__submit" disabled={loading || !agreement}>
+            {t('createAccountAction')}
+          </button>
+        </Form.Submit>
 
-      <div className="form__checkbox-wrapper">
-        <input
-          id="agreement"
-          className="form__checkbox"
-          type="checkbox"
-          checked={agreement}
-          onChange={(e) => setAgreement(e.target.checked)}
-        />
-        <label htmlFor="agreement">
-          {t('iAgreeCheckbox')}{' '}
-          <a href="#" target="_blank" className="form__link">
-            {t('termsAndConditions')}
-          </a>
-        </label>
-      </div>
+        {error && <span className="form__message">{error}</span>}
 
-      <Form.Submit asChild>
-        <button className="form__submit" disabled={loading || !agreement}>
-          {t('createAccountAction')}
+        <p className="form__divider">
+          <span className="form__line"></span>
+          <span>{t('orRegisterWith')}</span>
+          <span className="form__line"></span>
+        </p>
+
+        <button
+          className="form__button-google"
+          type="button"
+          onClick={handleGoogleSignup}
+          disabled={loading}
+        >
+          <GoogleIcon />
+          {loading ? t('loggingInStatus') : t('googleLoginOption')}
         </button>
-      </Form.Submit>
-
-      {error && <span className="form__message">{error}</span>}
-
-      <p className="form__divider">
-        <span className="form__line"></span>
-        <span>{t('orRegisterWith')}</span>
-        <span className="form__line"></span>
-      </p>
-
-      <button
-        className="form__button-google"
-        type="button"
-        onClick={handleGoogleSignup}
-        disabled={loading}
-      >
-        <GoogleIcon />
-        {loading ? t('loggingInStatus') : t('googleLoginOption')}
-      </button>
-    </Form.Root>
+      </Form.Root>
+    </SlideIn>
   );
 }
